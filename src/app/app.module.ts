d9ea8d863d5d6d4b0253e8f800b7fc4d7d64330e
Francesca Giordano <francesca020406@hotmail.com>
2017-11-09 23:27:52 -0500

    finally learning routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { FormsModule }   from '@angular/forms';   // <-- Import declaration
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyAboutComponent
   ],
   imports: [
-    BrowserModule
-  ],
+  BrowserModule,
+  FormsModule,
+  RouterModule.forRoot(routes)  //  <!-- "routes" is the array defined above
+],
   providers: [],
   bootstrap: [AppComponent]
 })
